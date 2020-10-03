8a75a759f4bbdeb2c19371e574b565478eb145e2
Chris Walters <cpwalters75@gmail.com>
2020-10-03 19:35:58 -0400

    product screen

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -6,6 +6,7 @@ const data = {
             category: 'Fruit',
             image: '/images/p1.jpg',
             price: 10,
+            countInStock: 0,
             rating: 4.5,
             numReviews:10,
             description: 'Freshly picked'
@@ -16,6 +17,7 @@ const data = {
             category: 'Fruit',
             image: '/images/p2.jpg',
             price: 103,
+            countInStock: 10,
             rating: 4.0,
             numReviews:10,
             description: 'Freshly picked'
@@ -26,6 +28,7 @@ const data = {
             category: 'Fruit',
             image: '/images/p3.jpg',
             price: 101,
+            countInStock: 50,
             rating: 5.0,
             numReviews:10,
             description: 'Freshly picked'
@@ -36,6 +39,7 @@ const data = {
             category: 'Fruit',
             image: '/images/p4.jpg',
             price: 10,
+            countInStock: 40,
             rating: 4.5,
             numReviews:10,
             description: 'Freshly picked'
@@ -46,6 +50,7 @@ const data = {
             category: 'Fruit',
             image: '/images/p5.jpg',
             price: 10,
+            countInStock: 30,
             rating: 4.5,
             numReviews:10,
             description: 'Freshly picked'
@@ -56,6 +61,7 @@ const data = {
             category: 'Fruit',
             image: '/images/p6.jpg',
             price: 10,
+            countInStock: 20,
             rating: 4.5,
             numReviews:10,
             description: 'Freshly picked'
