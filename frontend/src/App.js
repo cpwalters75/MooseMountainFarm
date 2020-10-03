a48cda301ec53f4a508ca891751db89756ec7168
Chris Walters <cpwalters75@gmail.com>
2020-10-03 16:48:58 -0400

    rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from './components/Product';
 import data from './data';
 
 function App() {
@@ -6,11 +7,11 @@ function App() {
     <div className="grid-container">
        <header className="row">
          <div>
-           <a className="brand" href="index.html">Moose Moutain Farm</a>
+           <a className="brand" href="/">Moose Moutain Farm</a>
          </div>
          <div>
-           <a href="cart.html">Cart</a>
-           <a href="signin.html">Sign In</a>
+           <a href="/cart">Cart</a>
+           <a href="/signin">Sign In</a>
          </div>
        </header>
       <main>
@@ -18,40 +19,7 @@ function App() {
             <div className="row center">
               {
                 data.products.map((product) => (
-                  <div key={product._id} className="card">
-                  <a href={`/product/${product._id}`}>
-                  
-                    <img className="medium" 
-                    src={product.image} 
-                    alt= {product.name}
-                    />
-                  </a>
-                  <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                    </a>
-  
-                    <div className="rating">
-                      <span> 
-                        <i className="fa fa-star"></i> 
-                        </span>
-                      <span>
-                         <i className="fa fa-star"></i> 
-                         </span>
-                      <span> 
-                        <i className="fa fa-star"></i> 
-                        </span>
-                      <span> 
-                        <i className="fa fa-star"></i> 
-                        </span>
-                      <span> 
-                        <i className="fa fa-star"></i> 
-                        </span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                  </div>
-
+                 <Product key={product.id} product={product}></Product>
                 ))
                 }
 
