8aa5d5dd5aa709191a72dd7966749ac56569c3ca
Chris Walters <cpwalters75@gmail.com>
2020-10-03 15:14:00 -0400

    react added

---
+++ frontend/src/data.js
@@ -0,0 +1,65 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Blueberries',
+            category: 'Fruit',
+            image: '/images/p1.jpg',
+            price: 10,
+            rating: 4.5,
+            numReviews:10,
+            description: 'Freshly picked'
+        },
+        {
+            _id: '2',
+            name: 'Apples',
+            category: 'Fruit',
+            image: '/images/p2.jpg',
+            price: 103,
+            rating: 4.0,
+            numReviews:10,
+            description: 'Freshly picked'
+        },
+        {
+            _id: '3',
+            name: 'Strawberries',
+            category: 'Fruit',
+            image: '/images/p3.jpg',
+            price: 101,
+            rating: 5.0,
+            numReviews:10,
+            description: 'Freshly picked'
+        },
+        {
+            _id: '4',
+            name: 'Blackberries',
+            category: 'Fruit',
+            image: '/images/p4.jpg',
+            price: 10,
+            rating: 4.5,
+            numReviews:10,
+            description: 'Freshly picked'
+        },
+        {
+            _id: '5',
+            name: 'Oranges',
+            category: 'Fruit',
+            image: '/images/p5.jpg',
+            price: 10,
+            rating: 4.5,
+            numReviews:10,
+            description: 'Freshly picked'
+        },
+        {
+            _id: '6',
+            name: 'Grapes',
+            category: 'Fruit',
+            image: '/images/p6.jpg',
+            price: 10,
+            rating: 4.5,
+            numReviews:10,
+            description: 'Freshly picked'
+        }
+    ]
+};
+export default data;
